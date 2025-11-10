bcb56cd0afcc52b75092b022c51f5f014fa3b854
Eldar Mukhtarov <mukhtaroveldar05@gmail.com>
2025-11-10 22:31:47 +0100

    Remove unused code

--- crawler/spiders/admission_en.py
+++ crawler/spiders/admission_en.py
@@ -3,52 +3,7 @@ import urllib
 import scrapy
 from scrapy.http import Response
 
-from dataclasses import dataclass
-from parsel.selector import SelectorList
 from ..items import AdmissionEnItem
-
-
-# UNUSED
-@dataclass
-class FnSTab:
-    path: list[str]
-    content: str
-
-
-# UNUSED
-def parse_FnS_tablist(response: Response, tablist: SelectorList) -> list[FnSTab]:
-    tablist_buttons = tablist.xpath(".//button")
-    tab_ids = tablist_buttons.xpath("./@aria-controls").getall()
-    tab_titles = tablist_buttons.xpath("./text()").getall()
-
-    tabs: list[FnSTab] = []
-
-    for tab_id, tab_title in zip(tab_ids, tab_titles):
-        tab_xpath = response.xpath(f'//*[@id="{tab_id}"]')
-
-        if len(tab_xpath) == 0:
-            continue
-
-        subtablist_xpath = tab_xpath.xpath('.//ul[@role="tablist"]')
-        if len(subtablist_xpath) == 0:
-            tabs.append(FnSTab([tab_title], tab_xpath.get()))
-            continue
-
-        tabs += [
-            FnSTab([tab_title] + new.path, new.content)
-            for new in parse_FnS_tablist(response, subtablist_xpath)
-        ]
-
-    return tabs
-
-
-# UNUSED
-def parse_fees_and_scholarships(response: Response) -> list[FnSTab]:
-    top_tablist = response.xpath(
-        '//ul[@role="tablist" and not(ancestor::div[contains(@class, "tab-content")])]'
-    )
-
-    return parse_FnS_tablist(response, top_tablist)
 
 
 class AdmissionEnSpider(scrapy.Spider):
